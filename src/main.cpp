e6529e7c97c56e32b23c2f8f53f8ca5d157fea3d
AV-Coding <villasenor.ariel@gmail.com>
2019-12-03 09:32:57 -0700

    working on button

--- src/main.cpp
+++ src/main.cpp
@@ -61,18 +61,18 @@ int main(){
       //const char v[] ={'a'};
       // v+='a';
       // begin transmission by passing in our slave address 0x53
-      beginTransmission(Address);
-      requestFrom(0x02, Address); // Requesting from address 0x05, the "Ambient Temperature"
-      x = (read() << 8); // set z to read()'s return value, shifted 8 bits to the left 
+      //beginTransmission(Address);
+      //requestFrom(0x02, Address); // Requesting from address 0x05, the "Ambient Temperature"
+      //x = (read() << 8); // set z to read()'s return value, shifted 8 bits to the left 
       // end the transmission
-      endTransmission();
+      //endTransmission();
       
       // begin transmission by passing in our slave address
-      beginTransmission(Address);
-      requestFrom(0x05, Address);// Requesting for DATAX0 (0X32)
-      x |= read();// OR'ing the bits returned from read() with the value currently in x
+      //beginTransmission(Address);
+      //requestFrom(0x05, Address);// Requesting for DATAX0 (0X32)
+      //x |= read();// OR'ing the bits returned from read() with the value currently in x
       // end the transmission
-      endTransmission();
+      //endTransmission();
       
       // begin transmission by passing in our slave address
       /*beginTransmission(Address);
@@ -114,8 +114,6 @@ int main(){
       writeString(arr);
       Serial.println(x);
       delayMs(1000);
-      
-
     }
     return 0;
 }
