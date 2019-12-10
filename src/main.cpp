5231a6445039a7d70e547e95a5a639d5ff64e568
AV-Coding <villasenor.ariel@gmail.com>
2019-12-09 18:07:43 -0700

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -34,40 +34,14 @@ int main(){
   initSwitchPB3();
   initLCD();
   initLED();
-  //initBeeper();
   sei();
   initPWMTimer3();
   initPWMTimer4();
   initPWMTimer5();
-  //initI2C();
 
-  float voltage;
-  int Address = 0x18; // This is the slave address
-  //int x = 75;
-  // int y=0;
   float temperature = 0;
-  int beeped = 0;
   Serial.begin(9600);
   Serial.flush();
-
-  // begin transmission by passing in our slave address 0x53
-  //beginTransmission(Address);
-  // write(0x01); //Writing using our power control bit
-  // write(0x05);
-  //write(0x00);
-  //write(0x08);
-  // end the transmission
-  //endTransmission();
-
-  // begin transmission by passing in our slave address 0x53
-  /*beginTransmission(Address);
-  write(0x2D); //Writing using our power control bit
-  write(0x08);
-  // end the transmission*/
-  //endTransmission();
-
-	// Serial.flush();
-     //char z[]  =" ";
 
     if (!tempsensor.begin()){
     Serial.println("Couldn't find MCP9808!");
@@ -77,70 +51,25 @@ int main(){
       float c = tempsensor.readTempC();
       temperature = c * 9.0 / 5.0 + 32;
        char arr[10] = "";
-      //const char v[] ={'a'};
-      // v+='a';
-      // begin transmission by passing in our slave address 0x53
-      //beginTransmission(Address);
-      //requestFrom(0x02, Address); // Requesting from address 0x05, the "Ambient Temperature"
-      //x = (read() << 8); // set z to read()'s return value, shifted 8 bits to the left 
-      // end the transmission
-      //endTransmission();
-      
-      // begin transmission by passing in our slave address
-      //beginTransmission(Address);
-      //requestFrom(0x05, Address);// Requesting for DATAX0 (0X32)
-      //x |= read();// OR'ing the bits returned from read() with the value currently in x
-      // end the transmission
-      //endTransmission();
-      
-      // begin transmission by passing in our slave address
-      /*beginTransmission(Address);
-      requestFrom(y1, Address);// Requesting for DATAY1 (0X34)
-      y = (read() << 8);// set y to read()'s return value, shifted 8 bits to the left 
-      // end the transmission
-      endTransmission();
-      
-      // begin transmission by passing in our slave address
-      beginTransmission(Address); 
-      requestFrom(y0, Address);// Requesting for DATAY0 (0X33)
-      y |= read(); // OR'ing the bits returned from read() with the value currently in y
-      // end the transmission
-      endTransmission();*/
-
-      //Print the X coordinate
-      // Serial.print("Temperature in Celsius is:");
-      // Serial.print(x);
-      // Serial.print("\t");
-
-      //Print the Y coordinate
-      /*Serial.print("Temperature in Farenheit is:");
-      Serial.print(y);
-      Serial.print("\t");*/
-      // temperature = x;
-      // char msg = (char)temperature;
 
         if(On == 0){
           turnOffFan();
-          //turnOffBeeper();
-          Serial.println(On);
         }
         else{
-      if(temperature >= 77){
+          /*If h*/
+        if(temperature >= 77){
         turnOffLED();
         delayMs(1000);
         IncFrequency(4000);
         turnOnLED(1);
         if(temperature >= 85 && temperature < 95){
           changeDutyCycleFan(4.0);
-          Serial.print(">=90");
         }
         else if(temperature >=95){
           changeDutyCycleFan(5.0);
-          Serial.print(">=100");
         }
         else{
           changeDutyCycleFan(3.0);
-          Serial.print(">=71 < 85");
         }
       }
       else if(temperature <= 70){
@@ -170,8 +99,6 @@ int main(){
       moveCursor(0,4);
       itoa(temperature,arr,10);
       writeString(arr);
-      Serial.print(temperature);
-      Serial.print("\t");
       delayMs(1000);
       Serial.println(On);
       break;
@@ -181,39 +108,9 @@ int main(){
       moveCursor(0,4);
       itoa(temperature,arr,10);
       writeString(arr);
-      Serial.print(temperature);
       delayMs(1000);
       break;
     }
-      
-      /*if(temperature > 80){
-        turnOnBeeper();
-        if(~beeped){
-          BeeperMakeNoise();
-          beeped = 1;
-        }
-        turnOffHeatPad();
-        turnOnFan();
-        changeFan(temperature);
-      }
-      else if(temperature < 70){
-        turnOnBeeper();
-        if(~beeped){
-          BeeperMakeNoise();
-          beeped = 1;
-        }
-        turnOffFan();
-        turnOnHeatPad();
-        changeHeatPad(temperature);
-      }
-      else{
-        turnOffHeatPad();
-        turnOffFan();
-        turnOffBeeper();
-        turnOffRedLED();
-        turnOffGreenLED();
-        beeped = 0;
-      }*/
     }
     return 0;
 }
@@ -228,17 +125,10 @@ ISR(PCINT0_vect){
       On = 0;
       turnOffLCD();
       turnOffLED();
-      //turnOffHeatPad();
-      //turnOffFan();
-      //turnOffBeeper();
     }
     else{
       On = 1;
       turnOnLCD();
-      //initLED();
-      //turOnHeadPad();
-      //turnOnFan();
-      //turnOnBeeper();
     }
 
   }
